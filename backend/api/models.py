e11cea51442fb3038c962faca2a3e237eefb0416
Lucas <prestoinker@gmail.com>
2025-02-11 13:55:58 -0300

    chore: config api base project

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
